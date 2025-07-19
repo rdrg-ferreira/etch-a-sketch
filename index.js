f9082929c6d47d24f0f1b34018cab7d7c8ccea8a
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2025-07-19 17:52:41 +0100

    added button to change grid size and some minor changesto program logic

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const container = document.querySelector(".container");
+const changeGridSizeButton = document.querySelector(".change-grid");
 
-function createGrid() {
-    for (let i = 0; i < (16 * 16); i++) {
+function createGrid(size) {
+    // create square divs
+    for (let i = 0; i < (size ** 2); i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
+        square.style.flex = `1 1 calc(100% / ${size})`;
         container.appendChild(square);
     }
+}
 
-    // add hover effect
+function addHoverEffect() {
     container.addEventListener("mouseover", (e) => {
         if (e.target.classList.contains("grid-square")) {
             e.target.classList.add("square-hover");
@@ -15,4 +19,20 @@ function createGrid() {
     });
 }
 
-createGrid();
+function changeGridSize() {
+    // get new size
+    let newSize = prompt("Enter the new number of squares per side for the new grid (max = 100)");
+
+    if (newSize === null) return;
+
+    while (parseInt(newSize) < 0 || parseInt(newSize) > 100) {
+        alert("That value is invalid for the grid size. Try again.");
+        newSize = prompt("Enter the new number of squares per side for the new grid:");
+    }
+
+    // remove square divs
+    container.textContent = "";
+
+    // create new grid
+    createGrid(newSize);
+}
